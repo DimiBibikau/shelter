830961882385d77ba4d2a6d36bb4514ac00393cc
DimiBibikau <79542820+DimiBibikau@users.noreply.github.com>
2022-06-05 23:20:18 +0200

    feat: mobile memenu start and pets json

--- main/script.js
+++ main/script.js
@@ -1,1 +1,25 @@
-dud
+const menu = document.querySelector(".hamburger-menu");
+const menuItems = document.querySelectorAll("nav-item");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".close-icon");
+const menuIcon = document.querySelector(".burger-menu");
+
+const burgerMenu = () => {
+    if (menu.classList.contains("show-menu")) {
+        menu.classList.remove("show-menu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else {
+        menu.classList.add("show-menu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+hamburger.addEventListener("click", burgerMenu);
+
+const renderPetShelter = () => {
+
+}
+
+renderPetShelter()
