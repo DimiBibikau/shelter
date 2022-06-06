c48b027de334d4d5875f6b2eac8961c7c4da5777
DimiBibikau <79542820+DimiBibikau@users.noreply.github.com>
2022-06-06 16:15:34 +0200

    feat: mobile menu

--- main/script.js
+++ main/script.js
@@ -1,22 +1,34 @@
-const menu = document.querySelector(".hamburger-menu");
-const menuItems = document.querySelectorAll("nav-item");
-const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".close-icon");
-const menuIcon = document.querySelector(".burger-menu");
+const menu = document.querySelector(".hamburger-navigation");
+const menuItems = document.querySelectorAll(".hamburger-nav-item");
 
-const burgerMenu = () => {
+const menuIcon = document.querySelector(".burger-menu");
+const closeIcon = document.querySelector(".burger-menu-vertical");
+
+// // const header = document.querySelector(".header");
+// const logo = document.querySelector(".logo")
+
+function burgerMenu () {
     if (menu.classList.contains("show-menu")) {
         menu.classList.remove("show-menu");
         closeIcon.style.display = "none";
         menuIcon.style.display = "block";
+
+        // logo.style.display = "block"
     } else {
         menu.classList.add("show-menu");
         closeIcon.style.display = "block";
         menuIcon.style.display = "none";
+        menu.style.display = "flex";
+
+        // logo.style.display = "none"
+        // header.style.display = "none"
+        
     }
 }
 
-hamburger.addEventListener("click", burgerMenu);
+menuIcon.addEventListener("click", burgerMenu);
+
+menuItems.forEach((menuItem) => menuItem.addEventListener("click", burgerMenu));
 
 const renderPetShelter = () => {
 
