e8446446a072b7479cb6850f541d8e97149a72b4
DimiBibikau <79542820+DimiBibikau@users.noreply.github.com>
2022-06-07 10:26:20 +0200

    feat: add dimm and clickout for the background

--- main/script.js
+++ main/script.js
@@ -6,23 +6,33 @@ const closeIcon = document.querySelector(".burger-menu-vertical");
 
 const logo = document.querySelector(".logo")
 
+const dimClickOut = document.querySelector(".dim-click-div");
+
 function burgerMenu () {
     if (menu.classList.contains("show-menu")) {
         menu.classList.remove("show-menu");
+
         closeIcon.style.display = "none";
+        dimClickOut.style.display = "none";
+
         menuIcon.style.display = "block";
-        logo.style.display = "block"
+        logo.style.display = "block";
+        
     } else {
         menu.classList.add("show-menu");
+        
         closeIcon.style.display = "block";
         menuIcon.style.display = "none";
         menu.style.display = "flex";
-        logo.style.display = "none"       
+        logo.style.display = "none";
+        
+        dimClickOut.style.display = "block";       
     }
 }
 
 menuIcon.addEventListener("click", burgerMenu);
 closeIcon.addEventListener("click", burgerMenu);
+dimClickOut.addEventListener("click",  burgerMenu)
 
 menuItems.forEach(
     function (menuItem) {
@@ -31,7 +41,11 @@ menuItems.forEach(
 );
 
 const renderPetShelter = () => {
-
+    const tagCreator = (tag, className, parentTag, indexParentTag = 0) => {
+        const element = document.createElement(tag);
+        element.classList.add(className);
+        document.querySelectorAll(parentTag)[indexParentTag].append(element);
+    }
 }
 
 renderPetShelter()
